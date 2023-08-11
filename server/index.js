65408f4cddcaa96ca882401eccd71e11acdfb373
John <johnudoumoh63@gmail.com>
2023-08-11 08:48:36 +0100

    sorted upload api

--- server/index.js
+++ server/index.js
@@ -1,17 +1,41 @@
 const express = require('express');
-const cloudinary = require('cloudinary').v2
 const app = express()
 require('dotenv').config()
-require('./config.js')
+const cloudinary = require('cloudinary').v2
+
+cloudinary.config({
+    secure: true,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello world</h1>')
 })
 
-app.get('/post', (req, res) => {
-    cloudinary.v2.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
-        { public_id: "olympic_flag" },
-        function (error, result) { res.send(result); });
+app.post('/post', async(req, res) => {
+        const body = req.body
+        // Use the uploaded file's name as the asset's public ID and 
+        // allow overwriting the asset with new versions\
+        console.log(body)
+        const options = {
+            use_filename: true,
+            unique_filename: false,
+            overwrite: true,
+        };
+
+
+        try {
+            // Upload the image
+            const result = await cloudinary.uploader.upload(body.imagePath, options);
+            console.log(result);
+            res.send(result.public_id);
+        } catch (error) {
+            console.error(error);
+        }
 })
 
 const PORT = process.env.PORT || 3002
